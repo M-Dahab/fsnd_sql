0b6afb18531f3f20e9bc931682d79359d2dc5534
M-Dahab <mohmmad.dahab@tamhub.com>
2018-10-29 08:59:24 +0300

    Add the third report (days on which failed requests are more than 1%)

--- report.py
+++ report.py
@@ -40,3 +40,27 @@ for idx, row in enumerate(cur.fetchall()):
     print('%d- %s — %d views.\n' % (idx + 1, row[0], row[1]))
 
 
+# Print the third report
+# (On which days did more than 1% of requests lead to errors?).
+cur.execute('''
+  select to_char(date, 'YYYY-MM-DD'), errors
+    from (
+      select date_trunc('day', time) as date,
+        round(
+          (count(case when status = '404 NOT FOUND' then 1 end)::float /
+          count(id) * 100)::numeric,
+          2
+        ) as errors
+      from log
+      group by date
+      order by errors desc
+    ) as errors_report
+    where errors > 1;
+''')
+print('\n\t\t# REPORT 3\n')
+print('\n\nDays with more than 1% error rate are:\n')
+
+for idx, row in enumerate(cur.fetchall()):
+    print(
+      '%d- %s — %s%% of requests failed.\n' % (idx + 1, str(row[0]), row[1])
+    )
